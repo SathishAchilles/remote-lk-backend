0e6d1a8127c28a07b328574c57a3f7622467a0d4
Sathish <reachtosathish.b@gmail.com>
2022-01-10 00:13:03 +0530

    build in-memory table

--- app/people_controller.rb
+++ app/people_controller.rb
@@ -11,6 +11,7 @@ class PeopleController
 
   def initialize(params)
     @params = params
+    @table = build_table
   end
 
   def normalize
@@ -18,9 +19,28 @@ class PeopleController
 
   private
 
-  attr_reader :params
+  attr_reader :params, :table
 
   def normalize_params
     params.slice(:dollar_format, :percent_format, :order)
+  end
+
+  def build_table
+    SUPPORTED_FORMATS.flat_map do |format_key, delimiter|
+      CSV.parse(normalize_params["#{format_key}_format".to_sym],
+                col_sep: delimiter,
+                headers: true,
+                header_converters: :symbol)
+         .map do |row|
+        row = row.to_hash
+        row.transform_values!(&:strip)
+        if CITY_ABBREVIATIONS.key?(row[:city])
+          row[:city] =
+            CITY_ABBREVIATIONS[row[:city]]
+        end
+        row[:birthdate] = Time.parse(row[:birthdate]).strftime('%-m/%-d/%Y')
+        row
+      end
+    end
   end
 end
