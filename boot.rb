ef2898043604e5a63cf35d03eae6b151c4b85bae
Sathish <reachtosathish.b@gmail.com>
2022-01-10 00:49:28 +0530

    boot all rb files under app directory

--- boot.rb
+++ boot.rb
@@ -1,1 +1,6 @@
-require './app/people_controller.rb'
+require 'bundler'
+PROJECT_ROOT = Bundler.root
+APP_ROOT = PROJECT_ROOT.join('app')
+CONFIG_ROOT = Bundler.root.join('config')
+
+Dir.glob(APP_ROOT.join('*/*.rb')).sort.each { |rb| require rb }
