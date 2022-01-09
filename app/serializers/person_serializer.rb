ec719ee13026fc2b0407b3b95f595e0fa298db58
Sathish <reachtosathish.b@gmail.com>
2022-01-10 02:26:16 +0530

    serialize persons

---
+++ app/serializers/person_serializer.rb
@@ -0,0 +1,13 @@
+class PersonSerializer
+  def initialize(people)
+    @people = people
+  end
+
+  def to_s
+    "#{people.first_name}, #{people.city}, #{people.birthdate.strftime('%-m/%-d/%Y')}"
+  end
+
+  private
+
+  attr_reader :people
+end
