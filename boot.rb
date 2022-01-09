3e83368639e114cca588519c1a2897d1b6e4d909
Sathish <reachtosathish.b@gmail.com>
2022-01-10 02:26:37 +0530

    read application configs at global level

--- boot.rb
+++ boot.rb
@@ -1,6 +1,8 @@
 require 'bundler'
+require 'yaml'
 PROJECT_ROOT = Bundler.root
 APP_ROOT = PROJECT_ROOT.join('app')
 CONFIG_ROOT = Bundler.root.join('config')
+APPLICATION_CONFIGS = YAML.load_file(CONFIG_ROOT.join('application.yml')).transform_keys(&:to_sym)
 
 Dir.glob(APP_ROOT.join('*/*.rb')).sort.each { |rb| require rb }
